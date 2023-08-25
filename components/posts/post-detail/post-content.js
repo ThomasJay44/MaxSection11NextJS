99fb1861e1e8e58a851257223717d33ad42cb081
tommysoprano <thomasjaymaly@gmail.com>
2023-08-25 17:33:00 -0400

    pos content paragraph

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,23 +1,56 @@
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
-
 
 function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <div className={classes.image}>
+    //       <Image
+    //         src={`/images/posts/${post.slug}/${image.url}`}
+    //         alt={image.alt}
+    //         width={600}
+    //         height={300}
+    //       />
+    //     </div>
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].type === 'image') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    }
+  };
+
   return (
-    <arcticle className={classes.content}>
+    <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>
         {post.content}
       </ReactMarkdown>
-    </arcticle>
+    </article>
   );
 }
 
 export default PostContent;
- 
