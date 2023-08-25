2da07088994875fef7894ff0928e1d62d047bcc8
tommysoprano <thomasjaymaly@gmail.com>
2023-08-25 16:32:49 -0400

    slug and posts utils

--- pages/posts/slug.js
+++ pages/posts/slug.js
@@ -1,9 +1,33 @@
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData } from '../../lib/posts-utils';
 
 function PostDetailPage() {
     return (
         <PostContent />
     )
+}
+
+export function getStaticProps(context) {
+    const { params } = context;
+    const { slug } = params;
+
+    const postData = getPostData(slug);
+    
+    return {
+        props: {
+            post: postData,
+        },
+        revalidate: 600,
+    };
+}
+
+export function getStaticPaths() {
+    
+
+    return {
+        paths: [],
+        fallback: false,
+    }
 }
 
 export default PostDetailPage
