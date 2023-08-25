2bbe05693f770b4b58f37745d42d94a516cb5c21
tommysoprano <thomasjaymaly@gmail.com>
2023-08-25 15:53:18 -0400

    get posts functions

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,8 @@
+import ReactMarkdown from "react-markdown";
+
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css";
+
 const DUMMY_POST = {
   slug: "getting-started-with-nextjs",
   title: "Getting Started with NextJS",
@@ -10,11 +15,14 @@ function PostContent() {
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
 
   return (
-    <arcticle>
+    <arcticle className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>
+        {DUMMY_POST.content}
+      </ReactMarkdown>
     </arcticle>
   );
 }
 
 export default PostContent;
+ 
---
+++ posts/getting-started-with-nextjs.md
@@ -0,0 +1,12 @@
+---
+title: 'Getting Started with NextJS'
+date: '2022-10-15'
+image: getting-started-nextjs.png
+excerpt: NextJS is a react framwork blah blah blah blah blah blah blah
+isFeatured: true
+---
+
+# This is a title
+
+This is some regular text with a [link] (https://google.com)
+
